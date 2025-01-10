b97ebee74ad178468a860b24232f8580de3f49dd
Melon999 <58394955+Melon999@users.noreply.github.com>
2025-01-11 01:17:38 +0800

    Update email.py

--- CEACStatusBot/notification/email.py
+++ CEACStatusBot/notification/email.py
@@ -23,16 +23,27 @@ class EmailNotificationHandle(NotificationHandle):
         
         # {'success': True, 'visa_type': 'NONIMMIGRANT VISA APPLICATION', 'status': 'Issued', 'case_created': '30-Aug-2022', 'case_last_updated': '19-Oct-2022', 'description': 'Your visa is in final processing. If you have not received it in more than 10 working days, please see the webpage for contact information of the embassy or consulate where you submitted your application.', 'application_num': '***'}
 
-        mail_title = '[CEACStatusBot] {} : {}'.format(result["application_num_origin"],result['status'])
-        mail_content = str(result)
+        mail_title = '美签状态监控: {}'.format(result['status'])
+
+        # 格式化邮件内容为 HTML 格式，使用 <br> 标签换行
+        mail_content_html = (
+            f"<p><strong>Time:</strong> {result['time']}</p>"
+            f"<p><strong>Visa Type:</strong> {result['visa_type']}</p>"
+            f"<p><strong>Status:</strong> <span style='color:red;'>{result['status']}</span></p>"
+            f"<p><strong>Case Created:</strong> {result['case_created']}</p>"
+            f"<p><strong>Case Last Updated:</strong> {result['case_last_updated']}</p>"
+            f"<p><strong>Description:</strong> {result['description']}</p>"
+            f"<p><strong>Application Number:</strong> {result['application_num']}</p>"
+            f"<p><strong>Original Application Number:</strong> {result['application_num_origin']}</p>"
+        )
 
         msg = MIMEMultipart()
-        msg["Subject"] = Header(mail_title,'utf-8')
+        msg["Subject"] = Header(mail_title, 'utf-8')
         msg["From"] = self.__fromEmail
         msg['To'] = ";".join(self.__toEmail)
-        msg.attach(MIMEText(mail_content,'plain','utf-8'))
+        msg.attach(MIMEText(mail_content_html, 'html', 'utf-8'))
 
-        smtp = SMTP_SSL(self.__hostAddress, self.__hostPort) # ssl登录
-        print(smtp.login(self.__fromEmail,self.__emailPassword))
-        print(smtp.sendmail(self.__fromEmail,self.__toEmail,msg.as_string()))
+        smtp = SMTP_SSL(self.__hostAddress, self.__hostPort)  # ssl登录
+        print(smtp.login(self.__fromEmail, self.__emailPassword))
+        print(smtp.sendmail(self.__fromEmail, self.__toEmail, msg.as_string()))
         smtp.quit()
